e88fde9094d5b1b2d5f8965feb847fe3e1816c34
Kyle Morehead <krmorehead@gmail.com>
2016-05-10 19:22:56 -0700

    Refactored Login Form to make sense, added firstname last name for each partner

--- client/login/login.js
+++ client/login/login.js
@@ -1,6 +1,8 @@
 angular.module("coupleFriends.LoginController", [])
 
 .controller("LoginController", function($scope, $interval){
+  $scope.loginData = {}
+  $scope.message = ""
   $scope.currentPhoto = "resources/doubleDating1.jpg"
   var currentPhotoIndex = 0;
   //maybe throw this into the node backend some how? start with a single image and then thrown the others across when the page is ready
@@ -12,7 +14,6 @@ angular.module("coupleFriends.LoginController", [])
   ]
 
   $interval(function(){
-    console.log ("testing", $scope.currentPhoto, currentPhotoIndex)
     if(currentPhotoIndex === photos.length - 1){
       currentPhotoIndex = 0
     } else {
@@ -20,4 +21,21 @@ angular.module("coupleFriends.LoginController", [])
     }
     $scope.currentPhoto = photos[currentPhotoIndex]
   }, 3000)
+
+  $scope.createAccount = function(){
+    if($scope.loginData.createAccount){
+      console.log("createAccount")
+    } else {
+      $scope.loginData.createAccount = true
+      console.log($scope.loginData)
+    }
+  }
+
+  var checkLoginDetails = function(){
+    if($scope.loginData.createAccount){
+
+    } else {
+
+    }
+  }
 })
