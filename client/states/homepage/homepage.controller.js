50cf39ff2611902df71461f08c8eea4abfbb0d1b
Kyle Morehead <kylemorehead@Kyles-MacBook-Pro.local>
2016-10-11 19:40:23 -0700

    Code cleanup

--- client/states/homepage/homepage.controller.js
+++ client/states/homepage/homepage.controller.js
@@ -11,20 +11,8 @@ angular.module("thePen.HomepageCtrl", [])
         vm.settings = Settings.getSettings();
 
         vm.authorData = AuthorData.getAuthorData($stateParams.displayUrl)
-        fillMenuBackground()
     }
     function go(slug) {
         $state.go("homepage." + slug)
     }
-
-    function fillMenuBackground() {
-        console.log(angular.element(document.querySelector('#menu'))[0].offsetHeight)
-        setTimeout(function(){
-            console.log(angular.element(document.querySelector('#menu'))[0].offsetHeight)
-        },1000)
-        vm.settings.menuFill = {
-            'height': angular.element(document.querySelector('#menu')).scrollHeight + 'px'
-        }
-    }
-
 })
