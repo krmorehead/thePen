7be8fd0579990879f7cb0a637b5d2d4d3912be76
Kyle Morehead <kylemorehead@Kyles-MacBook-Pro.local>
2016-10-12 16:51:36 -0700

    Added in check for dirty form

--- client/states/pages/pages.controller.js
+++ client/states/pages/pages.controller.js
@@ -2,18 +2,25 @@ angular.module("thePen.StoriesCtrl", [])
 
 .controller("StoriesCtrl", StoriesCtrl);
 
-function StoriesCtrl($stateParams, $state, AuthorData, UserData){
+function StoriesCtrl($scope, $stateParams, $state, AuthorData, UserData){
     var vm = this;
     vm.stateSlug = $stateParams.page;
     vm.editMode = false;
+    vm.isDirty = false;
 
     initialize()
+
 
     function initialize() {
         vm.userData = UserData.getUserData();
 
         vm.pageData = AuthorData.getPageData(vm.stateSlug);
         vm.onOwnPage = vm.userData.id === AuthorData.getAuthorData().id;
+        $scope.$watch("vm.pageData", function(){
+            if (vm.editMode) {
+                vm.isDirty = true;
+            }
+        }, true);
     }
 
 }
