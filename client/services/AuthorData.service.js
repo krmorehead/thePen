982d0355e3770ed0fa23ab5d9dd43d6867577c4f
Kyle Morehead <kylemorehead@Kyles-MacBook-Pro.local>
2016-10-12 17:32:22 -0700

    Added dynamic styling properties to pages template

--- client/services/AuthorData.service.js
+++ client/services/AuthorData.service.js
@@ -72,6 +72,10 @@
                     text: "Lorem ipsum dolor sit amet, sapien incididunt vestibulum dictum libero justo, a ante in sed fusce mauris pulvinar, erat quia quis semper, mauris habitasse enim aliquam. \nCondimentum libero amet, pretium massa nec ante, eleifend proin ultricies aliquam varius. Venenatis sit hendrerit ante hendrerit, id eu tempus id risus vulputate, aliquet enim. Donec imperdiet pulvinar ut quibusdam magna, suspendisse turpis litora turpis, sed erat lectus ligula donec quisque enim. Aliquet ut sed orci, vitae libero elementum, purus metus in a quis diam, urna in. Metus ipsum bibendum libero duis quam, risus nullam ut nec enim, placerat vitae diam ut cras fermentum a, arcu egestas etiam elit lorem tincidunt nonummy. Non auctor eu cras sodales elementum, ut auctor harum cursus soluta, quam augue nulla neque. Vel dis scelerisque nisl, pellentesque gravida at donec lorem nibh, metus eum porttitor magna cras scelerisque, quis nec sit vel, gravida posuere amet sed vehicula eget. Eros interdum non, laoreet donec praesent vel dapibus inceptos amet, magna ipsum ut ut amet. Ac sodales vivamus arcu sit, non adipiscing montes nascetur odio. Ultrices urna neque ipsum magnis. Ornare nunc neque purus est, lectus aliquam, hendrerit suspendisse sodales etiam ad mauris, parturient nec non at, aliquam nisl magna. Nonummy rhoncus fuga eu congue, nulla a et tincidunt ultrices mi. Ut morbi nam aliquam cras eum ante, sem interdum. Ultrices eget dui mattis perspiciatis justo vel. Curabitur elit at suspendisse iaculis lacus nibh. \n Praesent at mollis placerat ac, magni suscipit. Ipsum ante habitant quam id hymenaeos, ultrices orci sunt nec, turpis praesent maecenas nunc sem justo. Diam ut orci volutpat ornare lobortis metus. Ante praesent suscipit ipsum aenean, tellus vestibulum augue reprehenderit, scelerisque sem, a velit tellus purus facilisi nunc magnis, ut nullam eros molestiae donec arcu. Vestibulum molestie diam dignissim sodales, viverra mauris turpis eu, vitae elit sociis. Metus ac nisl amet odio fermentum, vitae magna consectetuer mauris, donec non lorem quis odio quam, sed enim, turpis ipsum adipiscing."
                 }],
                 settings: {
+                    header: {
+                        styles:{},
+                        'class':''
+                    },
                     body: {
                         'margin' : '5',
                         'background-color' : 'rgba(245, 241, 222, 0.8)',
@@ -80,6 +84,13 @@
                         'min-width': '30%',
                         'max-width': '60%',
                         'border-radius': '10px',
+                    },
+                    paragraphs: {
+                        styles:{
+                            'border-radius': '10px',
+                            'padding': '10',
+                        },
+                        'class': ''
                     }
                 },
             };
